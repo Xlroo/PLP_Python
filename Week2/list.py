296f6ff0f471750dda2996082dce7100f41525c7
Xlroo <80978847+Xlroo@users.noreply.github.com>
2025-05-07 14:15:36 +0300

    Week 2 work

---
+++ Week2/list.py
@@ -0,0 +1,20 @@
+# students_age = [23,27,25,30.5,"My age is young"]
+
+# print(students_age)
+# print(students_age[4])
+
+# print(students_age[-1])
+# print(students_age[-4])
+#slicing a list 
+# my_list = ["p","y","t","h","o","n"]
+#print(my_list[0:3])
+# print(my_list[2:])
+# print(my_list[:4])
+# v
+# print(my_list[2:])
+# print(my_list[-2:])
+
+numbers = [1,2,3,4,5,6,7]
+numbers.append(8)
+print(numbers)
+
---
+++ Week2/stack.py
@@ -0,0 +1,16 @@
+# stack =[]
+
+# stack.append(3)
+# stack.append(4)
+# stack.append(5)
+# stack.append(12)
+# stack.append(6)
+# stack.append(9)
+# print(stack)
+
+# print(stack[2])
+# print(stack.pop())
+
+stack_thanos = ['a','b','c','d','b']
+stack_thanos.remove('b')
+print(stack_thanos)
